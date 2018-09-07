1102b0083a4f0426860ea726c9e77c0beae1611c
cui <angrynaivecui@gmail.com>
2018-09-07 21:48:31 +0800

    use TextArea from antd to display danmakus

--- src/components/Danmakus.js
+++ src/components/Danmakus.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { Card } from 'antd';
+import { Card, Input, Tag } from 'antd';
+
+const { TextArea } = Input;
 
 class Danmakus extends Component {
   render() {
     return (
-      <Card
-        title='弹幕'
-      >
-        {
-          this.props.danmakus.map(danmaku => <p>
-            {danmaku.nl ? `贵族${danmaku.nl} ` : ''}{danmaku.bnn.length ? `[${danmaku.bnn}${danmaku.bl}] ` : ''}{danmaku.nn}(lv.{danmaku.level}): {danmaku.txt}
-          </p>)
-        }
-      </Card>
+      <TextArea
+        autosize='true'
+        value={this.props.danmakus.map(danmaku => {
+          return `${danmaku.nl ? `贵族${danmaku.nl} ` : ''}${danmaku.bnn.length ? `[${danmaku.bnn}${danmaku.bl}] ` : ''}${danmaku.nn}(lv.${danmaku.level}): ${danmaku.txt}`;
+        }).join('\n')} 
+      />
+
+      // <Card
+      //   title='弹幕'
+      // >
+      //   {
+      //     this.props.danmakus.map(danmaku => <p>
+      //       {danmaku.nl ? `贵族${danmaku.nl} ` : ''}{danmaku.bnn.length ? `[${danmaku.bnn}${danmaku.bl}] ` : ''}{danmaku.nn}(lv.{danmaku.level}): {danmaku.txt}
+      //     </p>)
+      //   }
+      // </Card>
     );
   }
 }
